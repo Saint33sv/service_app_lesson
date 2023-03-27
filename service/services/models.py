4305ccf6c64b1f9b229c4cbc8c92637cb8e11b07
saint <svatoslavrotonos@gmail.com>
2023-03-27 21:14:44 +0300

    Made some changes

--- service/services/models.py
+++ service/services/models.py
@@ -2,15 +2,12 @@ from django.db import models
 from django.core.validators import MaxValueValidator
 
 from clients.models import Client
-from services.tasks import set_price
+from services.tasks import set_price, set_comment
 
 
 class Service(models.Model):
     name = models.CharField(max_length=50)
-    full_price = models.PositiveIntegerField()
-
-    def __str__(self):
-        return f"{self.name}"
+    full_price = models.PositiveIntegerField() 
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -20,8 +17,12 @@ class Service(models.Model):
         if self.full_price != self.__full_price:
             for subscription in self.subscriptions.all():
                 set_price.delay(subscription.id)
+                set_comment.delay(subscription.id)
 
         return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Plan(models.Model):
@@ -34,10 +35,7 @@ class Plan(models.Model):
     discount_percent = models.PositiveIntegerField(default=0,
                                                    validators=[
                                                        MaxValueValidator(100)
-                                                       ])
-
-    def __str__(self):
-        return f"Plan: {self.plan_type} -{self.discount_percent}%"
+                                                       ]) 
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -47,8 +45,12 @@ class Plan(models.Model):
         if self.discount_percent != self.__discount_percent:
             for subscription in self.subscriptions.all():
                 set_price.delay(subscription.id)
+                set_comment.delay(subscription.id)
 
         return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Plan: {self.plan_type} -{self.discount_percent}%"
 
 
 class Subscription(models.Model):
@@ -59,6 +61,7 @@ class Subscription(models.Model):
     plan = models.ForeignKey(Plan, related_name='subscriptions',
                                on_delete=models.PROTECT)
     price = models.PositiveIntegerField(default=0)
+    comment = models.CharField(max_length=50, default='')
 
     def __str__(self):
         return f"{self.client.user}: {self.service}, {self.plan}"
